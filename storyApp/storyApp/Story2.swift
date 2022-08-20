432bd0c05c7938e6c412e2e679c9d0347430325c
user <user@MacBook-Pro-alkhas-b-user.local>
2022-08-20 14:00:13 +0300

    home work

---
+++ storyApp/storyApp/Story2.swift
@@ -0,0 +1,27 @@
+//
+//  Story2.swift
+//  storyApp
+//
+//  Created by user on 20/01/1444 AH.
+//
+
+import UIKit
+
+class Story2: UIViewController {
+
+    @IBOutlet weak var bodyStory2: UILabel!
+    @IBOutlet weak var titelStory: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titelStory.text = "GOLDEN"
+        
+        bodyStory2.text = "Gemma was only five minutes away from her parents’ hut, but the jungle had already taken on a different personality. It was thicker. The trees had grown taller. The ti plants shaded the green forest a sinister red. There was no doubt about it- the jungle was more dangerous here, and Gemma loved it. As a baby, Gemma’s parents had taken her on their expeditions over mountains, deserts, and vast seas. It was exciting, unpredictable, and deeply irresponsible. Now that they had settled down in the tropical rainforest, Gemma had to devise intricate plans just to sneak out for morning adventures"
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ storyApp/storyApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  storyApp
+//
+//  Created by user on 20/01/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var StroryTitle: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+      //  labelStoryUI.text = "قصة النمر والارنب"
+
+    
+    }
+    
+  
+}
+
