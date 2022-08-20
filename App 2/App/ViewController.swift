853a7e3f899800c1f2826732c65f15a8e2a46d33
DHAi <dhaii@DHAi.local>
2022-08-21 00:51:38 +0300

    complete

---
+++ App 2/App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by DHAi. on 21 محرم، 1444 هـ.
+//  Copyright © 1444 هـ DHAi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var NextButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NextButton.layer.cornerRadius = 16
+        
+    }
+
+
+}
+
